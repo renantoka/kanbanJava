59b686acb0a48b2e371cfc13852e2b602a147dd0
Renan Tokashiki <renantoka@icloud.com>
2021-07-01 16:45:09 -0400

    .

--- src/main/java/com/SEPLAG/projetoKanban/ProjetoKanbanApplication.java
+++ src/main/java/com/SEPLAG/projetoKanban/ProjetoKanbanApplication.java
@@ -5,14 +5,24 @@ import java.util.Locale;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
 @SpringBootApplication
+@Controller
 public class ProjetoKanbanApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoKanbanApplication.class, args);
+	}
+	
+	@RequestMapping ("/")
+	@ResponseBody
+	String home() {
+		return "index";
 	}
 
 	@Bean
